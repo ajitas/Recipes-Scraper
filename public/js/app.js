28db72e05c070428a8cbfc3a8e25aba1b6131497
Ajita Srivastava <ajitas4000@gmail.com>
2018-09-27 17:32:25 -0700

    adding inline comments

--- public/js/app.js
+++ public/js/app.js
@@ -1,24 +1,37 @@
+//on click of #scrape-recipes button
 $(document).on("click","#scrape-recipes", function(){
+  //a get request that scrapes the website and add the scraped data to database
     $.get("/scrape", function() {
+      //once the data has been scraped and added to database, reload the page to reflect the data
         location.reload(true);
       });
 })
 
+//on click of #favorite-recipes button
 $(document).on("click","#favorite-recipes", function(){
+  //get all the recipes that have favorite value true
     $.get("/favorites", function() {
+      //reload the page
         window.location.href="/favorites";
       });
 })
 
+//on click of #view-all-recipes button
 $(document).on("click","#view-all-recipes", function(){
+  //get all the recipes present in the database
     $.get("/", function() {
+      //reload the page
         window.location.href="/";
       });
 })
 
+//on click of .add-a-note button, shows modal
 $(document).on("click",".add-a-note", function(){
+    //clear the textarea of the modal
     $("#bodyinput").val("");
+    //get recipeID from the button data-id attribute
     var recipeID = $(this).attr("data-id");;
+    //get request to get the note for the recipeID
     $.ajax({
         method: "GET",
         url: "/recipes/" + recipeID +"/notes"
@@ -27,8 +40,9 @@ $(document).on("click",".add-a-note", function(){
         .then(function(data) {
           // The title of the article
           $("#exampleModalLongTitle").html("<h4>" + data.title + "</h4>")
+          //attach data-id attribute to the #savenote button
           $("#savenote").attr("data-id",data._id)
-          // If there's a note in the article
+          // If there's a note in the recipe
           if (data.note) {
             // Place the body of the note in the body textarea
             $("#bodyinput").val(data.note.body);
@@ -38,9 +52,11 @@ $(document).on("click",".add-a-note", function(){
 
 // When you click the savenote button
 $(document).on("click", "#savenote", function() {
+  //get the note body in a variable
     var noteData =  $("#bodyinput").val();
+    //hide the modal
     $("#exampleModalCenter").modal('hide');
-    // Grab the id associated with the article from the submit button
+    // Grab the id associated with the recipe from the submit button
     var recipeId = $(this).attr("data-id");
   
     // Run a POST request to change the note, using what's entered in the inputs
@@ -64,10 +80,12 @@ $(document).on("click", "#savenote", function() {
     $("#bodyinput").val("");
   });
   
-
+//on click of favorite button which is not yet added to favorites
 $(document).on("click",".add-to-favorites", function(){
+    //change the button's class to change it's color
     $(this).removeClass("add-to-favorites");
     $(this).addClass("added-to-favorites");
+    //get recipeID from button's data-id attribute
     var recipeId = $(this).data("id");
     //ajax call to update favorite to true
     $.ajax({
@@ -79,9 +97,12 @@ $(document).on("click",".add-to-favorites", function(){
       })
 })
 
+//on click of favorite button which is already added to favorites
 $(document).on("click",".added-to-favorites", function(){
+  //change the button's class to change it's color
     $(this).removeClass("added-to-favorites");
     $(this).addClass("add-to-favorites");
+    //get recipeID from button's data-id attribute
     var recipeId = $(this).data("id");
     //ajax call to update favorite to false
     $.ajax({
@@ -92,11 +113,14 @@ $(document).on("click",".added-to-favorites", function(){
         }
       })
 })
+
+//on click of favorite button which is already added to favorites on favorites page
 $(document).on("click",".added-to-favorites-favorite-recipes", function(){
+  //change the button's class to change it's color
     $(this).removeClass("added-to-favorites-favorite-recipes");
     $(this).addClass("add-to-favorites");
+    //get recipeID from button's data-id attribute
     var recipeId = $(this).data("id");
-    console.log("before clicking")
     //ajax call to update favorite to false
     $.ajax({
         method: "POST",
@@ -105,6 +129,7 @@ $(document).on("click",".added-to-favorites-favorite-recipes", function(){
           favorite: false
         }
       }).then(function(data){
+        //reloads the page to remove the recipe from the favorite page
         location.reload();
       })
 })
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
+//relevant packages
 var express = require("express");
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
 var request = require("request");
 var cheerio = require("cheerio");
 
+//port
 var PORT = process.env.PORT || 8080;
 
+//Initialize express
 var app = express();
 
 // Requiring our models for syncing
@@ -37,27 +40,47 @@ mongoose.connect(MONGODB_URI);
 
 
 
-// A GET route for scraping the echoJS website
+// A GET route for scraping the epicurious.com website
 app.get("/scrape", function(req, res) {
 
     // Make a request call to grab the HTML body from the site of your choice
     request("https://www.epicurious.com/search/?content=recipe&sort=newest", function(error, response, html) {
+
+        //load that into cheerio and save it to $ for a shorthand selector
         var $ = cheerio.load(html);
+
+        //get the number of recipes that we would be extracting from the website
         var len = $("article.recipe-content-card").length;
+
+        //for each recipe card in the html
         $("article.recipe-content-card").each(function(i, element) {
+
+            //scrape the title of the recipe
             var title = $(element).children("header.summary").children("h4.hed").children().text();
+            //scrape summary of the recipe
             var summary = $(element).children("header.summary").children("p.dek").text();
+            //scrape rating of the recipe
             var rating = $(element).children("header.summary").find("dd.rating").children("span").eq(0).text();
+            //scrape makeagain percentage for the recipe
             var makeagain = $(element).children("header.summary").find("dd.make-again-percentage").text();
+            //remove the last % from makeagain as we want to store it as number and not a string
             makeagain=makeagain.slice(0,makeagain.length-1);
+            //scrape the recipe link
             var link = "https://www.epicurious.com"+$(element).children("a.photo-link").attr("href");
 
+            //since picture was dynamically generated on this page and could not be scraped,
+            //To get the picture another request is made to the recipe link page
             request(link, function(error,response,html){
+
+                //load that into cheerio and save it to $ for a shorthand selector
                 var $ = cheerio.load(html);
+                //scrape the image
                 var image = $("img.photo.loaded").attr("srcset");
 
+                //empty object that will be used to make recipe object
                 var result ={};
                 
+                //add all scraped values to the object result
                 result.title= title,
                 result.summary=summary,
                 result.rating=parseInt(rating),
@@ -65,11 +88,12 @@ app.get("/scrape", function(req, res) {
                 result.link=link,
                 result.image=image
                 
-                // Create a new Article using the `result` object built from scraping
+                // Create a new Recipe using the `result` object built from scraping
                 db.Recipe.create(result).then(function(dbRecipe) {
                     // View the added result in the console
                     console.log(dbRecipe);
             
+                    //If all recipes are created in database, redirect to root route and display all the recipes from the database
                     if(i === len-1)
                         res.redirect("/");
                 }).catch(function(err) {
@@ -78,41 +102,49 @@ app.get("/scrape", function(req, res) {
                 });
             });
         })
-        //res.send("Scrape Complete");
     });
 });
 
 
-// Route for getting all Articles from the db
+// Route for getting all Recipes from the db
 app.get("/", function(req, res) {
     db.Recipe.find({}).then(function(dbData){
+        //if the data is sucessfully retrieved, render index.handlebars with this data
         res.render("index",{recipes:dbData})
       }).catch(function(err){
+          // If an error occurs, send it back to the client
         res.json(err);
       })
-  });
+});
 
-  app.get("/favorites", function(req, res) {
-    db.Recipe.find({favorite:true}).then(function(dbData){
+// Route for getting all Recipes that are favorite from the db
+app.get("/favorites", function(req, res) {
+db.Recipe.find({favorite:true}).then(function(dbData){
+    //if the data is sucessfully retrieved, render favorites.handlebars with this data
         res.render("favorites",{recipes:dbData})
-      }).catch(function(err){
+    }).catch(function(err){
+        // If an error occurs, send it back to the client
         res.json(err);
-      })
-  });
+    })
+});
 
-// Route for grabbing a specific Article by id, populate it with it's note
+// Route for grabbing a specific Recipe by id, populate it with it's note
 app.get("/recipes/:recipeID/notes", function(req, res) {
-
+    //find the recipe document for the given recipeID populated with it's note
     db.Recipe.findOne({"_id": req.params.recipeID}).populate("note").then(function(dbData){
+        //If data was successfully retrieved, send it as response to the client
       res.json(dbData)
     }).catch(function(err){
+        // If an error occurs, send it back to the client
       res.json(err);
     })
   });
 
-  // Route for saving/updating an Article's associated Note
+  // Route for saving/updating an Recipe's associated Note
 app.post("/recipes/:recipeID/notes", function(req, res) {
+    //create a new note for the selected recipeID with the value sent in body
     db.Note.create(req.body).then(function(dbNote){
+    //once the note has been created, find the recipeID in recipe collection and add the newly created note's id to it
       return db.Recipe.findOneAndUpdate({"_id":req.params.recipeID}, { $set: { note: dbNote._id } }, { new: true });
     }).then(function(dbRecipe) {
       // If the User was updated successfully, send it back to the client
@@ -122,16 +154,21 @@ app.post("/recipes/:recipeID/notes", function(req, res) {
       // If an error occurs, send it back to the client
       res.json(err);
     });
-  });
+});
 
+//updates the Recipe collection's document where id=:recipeID; sets favorite
 app.post("/recipes/:recipeID", function(req,res){
+    //find one document where id matches request paramater and set favorite key value
     db.Recipe.findOneAndUpdate({"_id":req.params.recipeID}, {$set:{ favorite: req.body.favorite }}, { new: true })
     .then(function(dbRecipe){
+        // If the Recipe was updated successfully, send it back to the client
         res.json(dbRecipe);
-    }).catch(function(err){
-        res.json(err);
     })
-})
+    .catch(function(err){
+        // If an error occurs, send it back to the client
+        res.json(err);
+    });
+});
 
   // Start the server
 app.listen(PORT, function() {
